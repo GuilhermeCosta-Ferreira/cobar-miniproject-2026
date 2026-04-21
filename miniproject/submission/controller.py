e39616e3a66259b8cc8a3fb7ad8e94d31e120b0f
Lily <xgilily01@gmail.com>
2026-04-21 15:03:04 +0200

    Level 0 working

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -1,6 +1,8 @@
 import numpy as np
 from miniproject.simulation import MiniprojectSimulation
 
+# our imports
+from .odor_tracking import odor_intensity_to_control_signal
 
 class Controller:
     def __init__(self, sim: MiniprojectSimulation):
@@ -9,10 +11,29 @@ class Controller:
 
         self.turning_controller = TurningController(sim.timestep)
 
+        # our inits
+        self.olfaction_smooth = None
+        self.alpha = 0.001 
+ 
+
     def step(self, sim: MiniprojectSimulation):
         # implement your control algorithm here
         olfaction = sim.get_olfaction(sim.fly.name)
-        # get other observations as needed
-        drives = np.array([1.0, 1.0])  # replace with your control logic
+        # smooth signal
+        self.process_olfaction(olfaction)
+            
+        # get control signals from olfaction
+        control_signals = self.olfaction_smooth  
+
+        drives = odor_intensity_to_control_signal(control_signals)  
         joint_angles, adhesion = self.turning_controller.step(drives)
         return joint_angles, adhesion
+    
+    def process_olfaction(self, signal):
+        if self.olfaction_smooth is None:
+            self.olfaction_smooth = signal
+        else:
+            self.olfaction_smooth = (
+                (1 - self.alpha) * self.olfaction_smooth + self.alpha * signal
+            )
+        return self.olfaction_smooth
