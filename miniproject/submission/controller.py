869359f0b238fc3c098a53dd9c1f167deb2ef0e4
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-05-19 15:28:16 +0200

    checkpoint: hybrid kinda working

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -30,6 +30,7 @@ from .vision import (
 class Controller:
     def __init__(self, sim: MiniprojectSimulation):
         self.turning_controller = HybridTurningController(sim.timestep)
+        #self.turning_controller = TurningController(sim.timestep)
         self.olfaction = Olfaction()
         self.wind = Wind(sim.mj_model)
         self.vision = Vision()
@@ -40,18 +41,19 @@ class Controller:
         current_step = sim._curr_step
 
         # OLFACTION
-        """
         olfaction = sim.get_olfaction(sim.fly.name)
         smooth_olfaction = self.olfaction.process_olfaction(olfaction)
         lateral_olfaction = average_olfaction_signal(smooth_olfaction)
         odor_drives = odor_intensity_to_control_signal(lateral_olfaction, attractive_gain=-800)
         self.olfaction.current_signal = odor_drives
 
+        """
         # WIND (will update odor information)
         wind = sim.get_antenna_data(sim.fly.name)
         wind_signal = self.wind.process_wind(wind)
 
         #wind_x = get_wind_velocity(wind)
+        """
 
         # VISION
         vision_signal = np.array([0.0, 0.0])
@@ -63,9 +65,10 @@ class Controller:
 
         # UPDATE THIS
         #updated_olfaction = update_olfaction(lateral_olfactation, wind_x)
-        control_signals = odor_drives + vision_signal + wind_signal
+        #control_signals = odor_drives + vision_signal + wind_signal
+        control_signals = odor_drives + vision_signal
 
         drives = damp_drives_for_rough_terrain(control_signals)
-        """
-        joint_angles, adhesion = self.turning_controller.step(sim)
+        joint_angles, adhesion = self.turning_controller.step(sim, drives)
+        #joint_angles, adhesion = self.turning_controller.step(drives)
         return joint_angles, adhesion
